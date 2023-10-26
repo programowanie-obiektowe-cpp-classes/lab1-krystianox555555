69e911bc221401511077702145879956b54160f6
krystianox555555 <149065400+krystianox555555@users.noreply.github.com>
2023-10-26 13:30:39 +0200

    Update Wektor2D.hpp
    wersja v1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,55 @@
-class Wektor2D
+#include <iostream>
+using namespace std;
+
+class informer
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+	informer()
+	{
+		std::cout << "konstrukcja informera ...\n";		//zadanie 3
+	}
+	~informer()
+	{
+		std::cout << "destrukcja informera ...\n";
+
+	}
+};
+
+class wektor2D
+{
+public:
+	int x;
+	int y;
+	double dlugosc()
+	{
+		return sqrt(x * x + y * y);
+	}
+
+	wektor2D(int wsporzednax, int wsporzednay)		// zadanie 2
+	{
+		x = wsporzednax;
+		y = wsporzednay;
+		std::cout << "Hello, oto wspolrzedne " << x << "!\n" << y;
+	}
+	~wektor2D()
+	{
+		std::cout << "Goodbye, " << x << "...\n" << y;
+	}
+private:
+	double setx(int x1) { x = x1; return 0; }			// zaddanie 4
+	double sety(int y1) { x = y1; return 0; }
+	double getx() { return x; }
+	double gety() { return y; }
+
+	wektor2D operator+(const wektor2D& other) //zadanie 5
+	const 
+	{
+		return wektor2D(x + other.x, y + other.y);
+	}	
+	wektor2D operator*(const wektor2D& other) //zadanie 5
+		const
+	{
+		return wektor2D(x*other.x, y*other.y);
+	}
+
 };
